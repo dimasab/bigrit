8017981d235f0fe72c9040bc03ff2dff0dd71d8d
Dimas <78547184+dimasab@users.noreply.github.com>
2025-07-26 11:53:18 +0700

    Create transformer.js

---
+++ TRANSFORMERS/transformer.js
@@ -0,0 +1,26 @@
+const autobots = [
+    "Optimus Prime",
+    "Bumblebee",
+    "Ratchet",
+    "Ironhide",
+    "Jazz",
+    "Prowl",
+    "Wheeljack",
+    "Arcee",
+    "Windblade"
+];
+const decepticons = [
+    "Megatron",
+    "Starscream",
+    "Soundwave",
+    "Shockwave",
+    "Thundercracker",
+    "Skywarp",
+    "Blitzwing",
+    "Bonecrusher",
+    "Blackout"
+];
+function printTransformers() {
+    console.log("wkkwkkkwkkkwkwkkw");
+}
+printTransformers();
